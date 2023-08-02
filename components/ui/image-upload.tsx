ac3512ab39d88a08d3c570db3c08a4c3cde5cc96
Naledi Sandamela <56083410+seymourpublic@users.noreply.github.com>
2023-08-02 21:03:59 +0200

    fix cloudinary

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="qokkn7vt">
         {({ open }) => {
           const onClick = () => {
             open();
