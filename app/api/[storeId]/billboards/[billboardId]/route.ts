022bf736253456e318ea7aa97b7215b8c3ae5a8b
Naledi Sandamela <56083410+seymourpublic@users.noreply.github.com>
2023-08-02 11:33:40 +0200

    billboard api doc

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -1,45 +1,58 @@
+// Import the necessary modules and libraries
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs";
-
 import prismadb from "@/lib/prismadb";
 
+// Handler for the GET request
 export async function GET(
   req: Request,
   { params }: { params: { billboardId: string } }
 ) {
   try {
+    // Check if the billboardId parameter is provided
     if (!params.billboardId) {
+      // If billboardId is missing, return a 400 Bad Request response
       return new NextResponse("Billboard id is required", { status: 400 });
     }
 
+    // Fetch the billboard data from the database using the provided billboardId
     const billboard = await prismadb.billboard.findUnique({
       where: {
         id: params.billboardId
       }
     });
-  
+
+    // Return the billboard data in JSON format
     return NextResponse.json(billboard);
   } catch (error) {
+    // If any error occurs during the process, log the error and return a 500 Internal Server Error response
     console.log('[BILLBOARD_GET]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 };
 
+// Handler for the DELETE request
 export async function DELETE(
   req: Request,
   { params }: { params: { billboardId: string, storeId: string } }
 ) {
   try {
+    // Get the authenticated user's ID from the request
     const { userId } = auth();
 
+    // Check if the user is authenticated
     if (!userId) {
+      // If not authenticated, return a 403 Forbidden response
       return new NextResponse("Unauthenticated", { status: 403 });
     }
 
-    if (!params.billboardId) {
-      return new NextResponse("Billboard id is required", { status: 400 });
+    // Check if the billboardId and storeId parameters are provided
+    if (!params.billboardId || !params.storeId) {
+      // If billboardId or storeId is missing, return a 400 Bad Request response
+      return new NextResponse("Billboard id and store id are required", { status: 400 });
     }
 
+    // Find the store associated with the billboardId and the authenticated user's ID
     const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
@@ -47,51 +60,56 @@ export async function DELETE(
       }
     });
 
+    // If the user is not the owner of the store, return a 405 Method Not Allowed response
     if (!storeByUserId) {
       return new NextResponse("Unauthorized", { status: 405 });
     }
 
+    // Delete the billboard from the database using the provided billboardId
     const billboard = await prismadb.billboard.delete({
       where: {
         id: params.billboardId,
       }
     });
-  
+
+    // Return the deleted billboard data in JSON format
     return NextResponse.json(billboard);
   } catch (error) {
+    // If any error occurs during the process, log the error and return a 500 Internal Server Error response
     console.log('[BILLBOARD_DELETE]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 };
 
 
+// Handler for the PATCH request
 export async function PATCH(
   req: Request,
   { params }: { params: { billboardId: string, storeId: string } }
 ) {
   try {   
+    // Get the authenticated user's ID from the request
     const { userId } = auth();
 
+    // Get the request body data as JSON
     const body = await req.json();
     
+    // Destructure the label and imageUrl properties from the request body
     const { label, imageUrl } = body;
     
+    // Check if the user is authenticated
     if (!userId) {
+      // If not authenticated, return a 403 Forbidden response
       return new NextResponse("Unauthenticated", { status: 403 });
     }
 
-    if (!label) {
-      return new NextResponse("Label is required", { status: 400 });
+    // Check if the label, imageUrl, billboardId, and storeId parameters are provided
+    if (!label || !imageUrl || !params.billboardId || !params.storeId) {
+      // If any required parameter is missing, return a 400 Bad Request response
+      return new NextResponse("Label, Image URL, Billboard id, and Store id are required", { status: 400 });
     }
 
-    if (!imageUrl) {
-      return new NextResponse("Image URL is required", { status: 400 });
-    }
-
-    if (!params.billboardId) {
-      return new NextResponse("Billboard id is required", { status: 400 });
-    }
-
+    // Find the store associated with the billboardId and the authenticated user's ID
     const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
@@ -99,10 +117,12 @@ export async function PATCH(
       }
     });
 
+    // If the user is not the owner of the store, return a 405 Method Not Allowed response
     if (!storeByUserId) {
       return new NextResponse("Unauthorized", { status: 405 });
     }
 
+    // Update the billboard data in the database using the provided billboardId
     const billboard = await prismadb.billboard.update({
       where: {
         id: params.billboardId,
@@ -112,9 +132,11 @@ export async function PATCH(
         imageUrl
       }
     });
-  
+
+    // Return the updated billboard data in JSON format
     return NextResponse.json(billboard);
   } catch (error) {
+    // If any error occurs during the process, log the error and return a 500 Internal Server Error response
     console.log('[BILLBOARD_PATCH]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
