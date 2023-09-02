d89105b93d94187dea3c3ed9405a863d184e875c
Naledi Sandamela <56083410+seymourpublic@users.noreply.github.com>
2023-09-02 18:19:42 +0200

    fix api issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from 'next/font/google'
+//import { Inter } from 'next/font/google'
+import { Quicksand } from 'next/font/google'
 
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToastProvider } from '@/providers/toast-provider'
 import { ThemeProvider } from '@/providers/theme-provider'
 
+
+
+
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Quicksand({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Dashboard',
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { twMerge } from "tailwind-merge"
 import clsx from 'clsx';
+import { ClassValue } from "class-variance-authority/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
